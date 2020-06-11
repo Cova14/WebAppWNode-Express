6f31dc361f4b59d79799682d51e7ae4aa8a32d6c
Cova14 <arty1498@hotmail.com>
2020-06-10 22:41:15 -0500

    Using debug and morgan uwu

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-var express = require('express')
-var chalk = require('chalk')
+var express = require('express');
+var chalk = require('chalk');
+var debug = require('debug')('index');
+var morgan = require('morgan');
 
 var app = express();
 
+app.use(morgan('tiny'));
 app.get('/', (req, res) => {
-  res.send('Holawa uwu')
-})
+  res.send('Holawa uwu');
+});
 
 app.listen(8000, () => {
-  console.log(`Listening on port ${chalk.green('8000')}`)
-})
+  debug(`Listening on port ${chalk.green('8000')}`);
+});
